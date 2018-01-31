2c90fa4810ce377099257b32fabd15b4dbcc0fe7
serpient <littlegangwolf@gmail.com>
2018-01-31 00:19:36 -0800

    connects from mongodb to heroku

--- nodeSimpleExample.js
+++ nodeSimpleExample.js
@@ -1,9 +1,8 @@
 // MONGODB_URI => mongodb://heroku_rvlt8l4c:2khaoveg93q4afkl21cvtlo7g5@ds117148.mlab.com:17148/heroku_rvlt8l4c
-
 var MongoClient = require('mongodb').MongoClient;
-
+const util = require('util');
+var Db = require('mongodb').Db;
 // Create seed data
-
 var seedData = [
     {
         text: "Hello World"
@@ -14,17 +13,20 @@ var seedData = [
 ]
 
 var uri = 'mongodb://heroku_rvlt8l4c:2khaoveg93q4afkl21cvtlo7g5@ds117148.mlab.com:17148/heroku_rvlt8l4c';
+const dbName = 'heroku_rvlt8l4c';
 
 MongoClient.connect(uri, function (err, client) {
     if (err) throw err;
     console.log("Successfully connected to server");
 
-    const testDB = client.db('test');
-    console.log(testDB);
-    testDB.insertMany(seedData);
-    // testDB.forEach(function(doc) {
-    //     console.log(doc.text);
-    // });
+    var db = client.db(dbName);
+
+    db.collection('hello').insert(seedData);
+
+    db.collection('hello').find().forEach( function(doc) {
+        console.log(doc.text);
+    });
+
     client.close(function (err) {
         if (err) throw err;
     });
@@ -32,3 +34,5 @@ MongoClient.connect(uri, function (err, client) {
 
     console.log("Inserted and printed new data");
 })
+
+
